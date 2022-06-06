6e06589b8b8babe392758ec489eca5600772ee62
Alamaguay <49161468+AndersCFR@users.noreply.github.com>
2022-06-06 01:07:11 -0500

    hi

---
+++ app/src/main/java/com/andersonCardenas/myapplication0/ejercicio.kt
@@ -0,0 +1,11 @@
+package com.andersonCardenas.myapplication0
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ejercicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio)
+    }
+}
